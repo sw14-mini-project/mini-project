b04641f5e809d5f338a3bf14072fa1c9a1a8226c
K1A2 <jckim0307@gmail.com>
2023-04-22 19:17:10 +0900

    [FEAT] navbar item 간격 가변 처리

--- src/components/navbar/BottomNavigationBar.vue
+++ src/components/navbar/BottomNavigationBar.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="root-bottom-navbar">
-
+    <NavbarItem label="채팅" icon="mdi-message-text"></NavbarItem>
+    <NavbarItem label="검색" icon="mdi-account-search"></NavbarItem>
+    <NavbarItem label="설정" icon="mdi-cog"></NavbarItem>
   </div>
 </template>
 
 <script>
+import NavbarItem from "@/components/navbar/NavbarItem";
 export default {
-  name: "BottomNavigationBar"
+  name: "BottomNavigationBar",
+  components: {NavbarItem}
 }
 </script>
 
---
+++ src/components/navbar/NavbarItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="navbtn-root">
+    <v-icon :icon="icon" class="navbtn-icon"></v-icon>
+    <div class="navbtn-label">{{ label }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavbarItem",
+  props: {
+    label: {
+      type: String,
+      default: "라벨"
+    },
+    icon: {
+      type: String,
+      default: "mdi-vuetify"
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../../css/navbar/navbar-item.scss";
+</style>
