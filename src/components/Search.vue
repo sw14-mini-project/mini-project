3ae8275c18701c60561c056fe6cb5028f997719c
dev-green-flamingo <devgflamingo@gmail.com>
2023-04-26 01:54:29 +0900

    [TEST] bottom sheet 테스트 코드
    bottom sheet을 사용하는 샘플 코드 입니다. 테스트 유닛을 작성하는 부분이 없어 commit message로 남깁니다.
    
    BREAKING CHANGE: 이 커밋은 단순한 샘플 코드이므로 실제로 구현할 시 이 커밋을 drop하고 사용해주시길 바랍니다

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -3,6 +3,11 @@
     <template v-slot:content>
     <!--   여기 안에 구현하시면 됩니다.   -->
       <div style="color: white">search</div>
+      <div>
+        <!-- bottom sheet 샘플 코드입니다. 상세 페이지에 병합할 때 사용하세요!-->
+        <button @click="showBottomSheet=true">Bottom Sheet 열기</button>
+        <bottom-sheet :show="showBottomSheet" :user="user" @close="showBottomSheet = false"/>
+      </div>
     </template>
   </DefaultPage>
 </template>
@@ -11,16 +16,36 @@
 import DefaultPage from "@/components/DefaultPage";
 import {auth} from "@/plugins/firebase";
 import {gotoPage} from "@/js/route";
+import BottomSheet from "@/components/BottomSheet.vue";
+import {ref} from "vue";
 export default {
   name: "SearchComponent",
-  components: {DefaultPage},
+  components: {BottomSheet, DefaultPage},
   setup() {
     auth.onAuthStateChanged((user) => {
       if (user === null) {
         gotoPage("login")
       }
-    })
-  }
+    });
+    const bottomSheetOpen = ref(false);
+
+    return {
+      bottomSheetOpen
+    };
+  },
+  data() {
+    return {
+      showBottomSheet: false,
+      // mock 데이터입니다. user 객체가 어떤 정보를 가지는지 알리기 위해 작성하였습니다. 상세페이지와 병합하면 삭제하세요.
+      user: {
+        nickname: 'test name',
+        description: 'help',
+        location: 'Seoul',
+        phoneNumber: '010-1234-5678',
+        position: 'backend',
+      },
+    };
+  },
 }
 </script>
 
